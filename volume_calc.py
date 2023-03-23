e659e015adb58b16466c8519346d3556aefd5bad
Jewon Shin <luke0126@naver.com>
2023-03-23 18:30:31 +0900

    Homework #1 volume_calc.py 수정

--- volume_calc.py
+++ volume_calc.py
@@ -1,11 +1,23 @@
-length  = input("가로:")
-width = input("세로:")
-height = input("높이:")
-
-length = float(length)
-width = float(width)
-height = float(height)
+length = float(input("가로:"))
+width = float(input("세로:"))
+height = float(input("높이:"))
 
 volume = length * width * height
 
-print("박스의 부피는", volume, "입니다.")
+print("Volume:", volume)
+
+total_length = length + width + height
+
+if total_length <= 80:
+    charge = 5000
+elif total_length <= 100:
+    charge = 8000
+elif total_length <= 120:
+    charge = 10000
+elif total_length <= 160:
+    charge = 13000
+else:
+    charge = "unavailable"
+
+print("Total length:", total_length)
+print("Charge:", charge)
